9a32aeebb21b69960e51a901b8b79434babc8e0c
Italo Chagas <felipe.italo@hotmail.com>
2026-04-12 11:54:45 -0300

    chore(app): remove legacy hooks (#260)

--- hooks/mutations/use-auth-mutations.ts
+++
@@ -1,8 +0,0 @@
-export {
-  useConfirmEmailMutation,
-  useForgotPasswordMutation,
-  useLoginMutation,
-  useRegisterMutation,
-  useResendConfirmationMutation,
-  useResetPasswordMutation,
-} from "@/features/auth/hooks/use-auth-mutations";
